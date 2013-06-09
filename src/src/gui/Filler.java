b986e9e018fe577e39f07bb466d12230034de575
David J Waggener <davidi@wettechsupport.com>
2013-06-09 13:30:34 -0400

    Not really necessary...
    Just so Github recognizes the folder structure.

---
+++ src/src/gui/Filler.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+//Just so Github will recognize the goddamn change
+/**
+ *
+ * @author Davidi
+ */
+public class Filler {
+    
+}
