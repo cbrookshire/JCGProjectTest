71f145d6cf7be20db2c5854d756e1d292c9d92b0
David Johnathan Waggener <prophetangel@gmail.com>
2013-06-25 22:53:08 -0400

    Uploading initial GUI
    When loading into Java, do not forget the .form files or you will not be
    able to edit the forms.
    Additionally, in the Netbeans editor, the buttons will only show up blue
    around the edges.  In the real program they're entirely blue.

---
+++ src/src/MainClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Davidi
+ */
+
+import gui.BaseJFrame;
+
+public class MainClass {
+    public static void main(String[] args)
+    {
+        BaseJFrame inst = BaseJFrame.getInstance();
+        inst.setVisible(true);
+    }
+}
---
+++ src/src/gui/BaseJFrame.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import gui.*;
+
+/**
+ *
+ * @author Davidi
+ */
+public class BaseJFrame extends JFrame {
+    private JPanel content = null;
+    private static BaseJFrame instance;
+    
+    public BaseJFrame()
+    {
+        FlowLayout layout = new FlowLayout();
+        this.setLayout(layout);
+        this.setSize(400, 300);
+        setContent(new OpeningJPanel(), "Main Page");
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public void setContent(JPanel c, String title)
+    {
+        if(content != null)
+        {
+            content.setVisible(false);
+            this.remove(content);
+        }
+        
+        content = null;
+        content = c;
+        
+        this.add(content);
+        content.setVisible(true);
+        
+        this.setTitle(title);
+    }
+    
+    public static BaseJFrame getInstance()
+    {
+        if(instance == null)
+            instance = new BaseJFrame();
+        
+        return instance;
+    }
+}
